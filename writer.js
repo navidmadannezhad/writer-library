6f6f1d11c898c35dcfb2ff98233b8b3721f8edda
navid.mnzh <navidproject283@gmail.com>
2021-07-01 16:21:11 +0430

    better structured, but how to add cursor?

--- writer.js
+++ writer.js
@@ -1,39 +1,45 @@
 /* به نام خداوند جان و خرد. کزین اندیشه برتر نگذرد */
 
-class Writer{
-    constructor(element, str, speed, state){
+class TypeWriter{
+    constructor({element, str, speed, state, direction}){
         this.element = element;
         this.str = str;
         this.speed = speed;
         this.state = state;
-        this.writerCompleted = false;
+        this.direction = direction;
+
+        this.strCompleted = false;
+        //this.cursor = '|';
     }
 
     init(){
         this.makeElementEmpty();
+        this.setElementProperties();
 
             if(this.state == 'onPageLoad'){
                 document.load = this.writeWord();
+
             }else if(this.state == 'onPageScroll'){
-                console.log('found');
                 window.addEventListener('scroll', () => {
-                    if(window.scrollY + window.innerHeight > this.element.offsetTop + this.element.offsetHeight && !this.writerCompleted){
+                    if(window.scrollY + window.innerHeight > this.element.offsetTop + this.element.offsetHeight && !this.strCompleted){
                         this.writeWord();
                         this.writerCompleted = true;
-                        console.log('must');
-                    }else{
-                        console.log({
-                            'scrollY': window.scrollY,
-                            'sum': this.element.offsetTop + this.element.offsetHeight
-                        });
                     }
                 })
             }
     }
 
+    setElementProperties(){
+        this.element.style.direction = this.direction;
+    }
+
     makeElementEmpty(){
         this.element.innerText = null;
     }
+
+    // addBlinkingCursor(){
+
+    // }
 
     writeWord(){
         let strLength = this.str.length;
