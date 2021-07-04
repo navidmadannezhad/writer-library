039e7e8f4d96e200c8324b7d069243b52955d3ad
navidmadannezhad <navidproject283@gmail.com>
2021-07-04 14:14:09 +0430

    cursor added. but only in the end of typer

--- writer.js
+++ writer.js
@@ -3,13 +3,13 @@
 class TypeWriter{
     constructor({element, speed, state, direction}){
         this.element = element;
-        this.str = element.innerText;
+        this.str = element.innerHTML;
         this.speed = speed;
         this.state = state;
         this.direction = direction;
 
         this.writterCompleted = false;
-        //this.cursor = '|';
+        this.cursor = '<span>|</span>';
     }
 
     init(){
@@ -28,7 +28,7 @@ class TypeWriter{
                 })
             }
     }
-
+ 
     setElementProperties(){
         this.element.style.direction = this.direction;
     }
@@ -37,9 +37,20 @@ class TypeWriter{
         this.element.innerText = null;
     }
 
-    // addBlinkingCursor(){
-
+    // setBlinkingCursor(){
+    //     console.log(this.blinkingCursor());
+    //     this.str = this.str + this.blinkingCursor();
     // }
+
+    blinkingCursor(){
+        let cursorElement = document.createElement('span');
+        cursorElement.classList.add('blinking-cursor');
+        cursorElement.innerHTML = '|';
+        return cursorElement;
+    }
+
+    //use the dom parser, console the created element to see how it is diplayed in console and how to add class and content to it
+    // outerHTML of the new element must be placed inside the innerHTML of the render text
 
     writeWord(){
         let strLength = this.str.length;
@@ -48,6 +59,9 @@ class TypeWriter{
                 this.element.innerHTML = this.element.innerHTML + this.str.charAt(i);
             }, this.speed*i);
         }
+        setTimeout(() => {
+            this.element.appendChild(this.blinkingCursor());
+        }, strLength*this.speed);
     }
 
    
